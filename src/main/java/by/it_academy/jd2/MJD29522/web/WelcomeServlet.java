9389f515e97e01727c8672b5d180e9dd6e645ec0
Pavel Klimenko <Horvard777@mail.ru>
2022-12-11 20:32:36 +0200

    remove WelcomeServlet

--- src/main/java/by/it_academy/jd2/MJD29522/web/WelcomeServlet.java
+++ src/main/java/by/it_academy/jd2/MJD29522/web/WelcomeServlet.java
@@ -8,15 +8,18 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet(name = "ServletVelcome", urlPatterns = {"/hello"})
+@WebServlet(name = "HelloWorldServlet", urlPatterns = {"/welcome"})
 
-public class ServletVelcome extends HttpServlet {
+public class WelcomeServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req,
                          HttpServletResponse resp)
             throws ServletException, IOException {
 
         PrintWriter writer = resp.getWriter();
-        writer.write("Добро пожаловать на голосование!");
-        }
+        writer.write("<p>Dear voter! Welcome to vote!</p>");
+        writer.write("<p>You can vote for only one singer</p>");
+        writer.write("<p>It is also necessary to vote for the genres you like (from 3 to 5)</p>");
+        writer.write("<p>And write about yourself</p>");
+    }
 }
