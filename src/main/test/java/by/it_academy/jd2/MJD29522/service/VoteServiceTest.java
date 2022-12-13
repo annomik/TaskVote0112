b3331a2f1540204459a1a7d632035e8230c7d82d
Dzmitry Bulavitski <dm_bax@mail.ru>
2022-12-13 15:56:30 +0300

    Добавил 61 строку в pom файле и пробный тест

---
+++ src/main/test/java/by/it_academy/jd2/MJD29522/service/VoteServiceTest.java
@@ -0,0 +1,19 @@
+package by.it_academy.jd2.MJD29522.service;
+
+import by.it_academy.jd2.MJD29522.dao.VoteDao;
+import by.it_academy.jd2.MJD29522.dto.SingerDTO;
+import by.it_academy.jd2.MJD29522.dto.SingerID;
+import by.it_academy.jd2.MJD29522.dto.VoteDTO;
+import by.it_academy.jd2.MJD29522.service.fabrics.VoteServiceSingleton;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class VoteServiceTest {
+    @Test
+    public void validationTest1(){
+        Assertions.assertEquals(1,1);
+    }
+}
