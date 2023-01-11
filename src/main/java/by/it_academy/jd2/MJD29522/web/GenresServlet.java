f531391c0d6483508ae8595da3bcdacb0689847f
Dzmitry Bulavitski <dm_bax@mail.ru>
2023-01-11 19:37:43 +0300

    В GenresServlet добавид метод doPost с возможностью обновления жанра!

--- src/main/java/by/it_academy/jd2/MJD29522/web/GenresServlet.java
+++ src/main/java/by/it_academy/jd2/MJD29522/web/GenresServlet.java
@@ -92,5 +92,33 @@ public class GenresServlet extends HttpServlet {
             service.add(nameForAdd);
             writer.write("<p>Вы добавили жанр " + nameForAdd + " по id: " + genres.size() + "</p>");
         }
+        if(mapParametrs.containsKey(UPDATE_ID) && mapParametrs.containsKey(UPDATE_NEW_NAME)){
+            String[] updateId = mapParametrs.get(UPDATE_ID);
+            String[] newName =  mapParametrs.get(UPDATE_NEW_NAME);
+
+            if(updateId == null && newName == null) {
+                throw new IllegalArgumentException("Id жанра или название нового жанра для обновления не переданы");
+            }
+            if(updateId.length > 1 && newName.length > 1){
+                throw new IllegalArgumentException("Жанры можно обновлять только по одному");
+            }
+            if(updateId[0] == null || updateId[0].isBlank() || newName[0] == null || newName[0].isBlank()){
+                throw new IllegalArgumentException("Обязательно необходимо ввести Id и новое название жанра " +
+                        "для обновления");
+            }
+            if(!NumberUtils.isDigits(updateId[0])){
+                throw new IllegalArgumentException("В поле updateId необходимо ввести число для обновления жанра по id");
+            }
+
+            int idForUpdate = Integer.parseInt(updateId[0]);
+            String name = newName[0];
+
+            if(service.exist(idForUpdate)){
+                String genreForUpdate = genres.get(idForUpdate - 1).getName();
+                service.update(idForUpdate, name);
+                writer.write("<p>Вы обновили название жанра " + genreForUpdate + " по id: " + idForUpdate +
+                        " на название: " + name + "</p>");
+            }
+        }
     }
 }
