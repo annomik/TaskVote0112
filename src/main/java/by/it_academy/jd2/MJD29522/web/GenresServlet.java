1df0b386397dfa8af938200d0ef164d13dfbf0ba
Dzmitry Bulavitski <dm_bax@mail.ru>
2023-01-14 19:28:11 +0300

    Добавил методы doPut, doDelete в GenresServlet!

--- src/main/java/by/it_academy/jd2/MJD29522/web/GenresServlet.java
+++ src/main/java/by/it_academy/jd2/MJD29522/web/GenresServlet.java
@@ -49,72 +49,90 @@ public class GenresServlet extends HttpServlet {
 
         PrintWriter writer = resp.getWriter();
 
-        if(mapParametrs.containsKey(DELETE_ID)){
-            String[] deleteGenre = mapParametrs.get(DELETE_ID);
-            if(deleteGenre == null) {
-                throw new IllegalArgumentException("id жанра для удаления не передан");
-            }
-            if(deleteGenre.length > 1){
-                throw new IllegalArgumentException("Жанры можно удалять только по одному");
-            }
-            if(deleteGenre[0] == null || deleteGenre[0].isBlank() || !NumberUtils.isDigits(deleteGenre[0])){
-                throw new IllegalArgumentException("Необходимо ввести число, для удаления жанра по id");
-            }
-            int idForDelete = Integer.parseInt(deleteGenre[0]);
-
-            if(service.exist(idForDelete)){
-                service.delete(idForDelete);
-                writer.write("<p>Вы удалили жанр по id: " + idForDelete + "</p>");
+        String[] addGenre = mapParametrs.get(ADD);
+        if(addGenre == null) {
+            throw new IllegalArgumentException("Название нового жанра для добавления не передано");
+        }
+        if(addGenre.length > 1){
+            throw new IllegalArgumentException("Жанры можно добавлять только по одному");
+        }
+        if(addGenre[0] == null || addGenre[0].isBlank()){
+            throw new IllegalArgumentException("Необходимо ввести название жанра для добавления");
+        }
+        String nameForAdd = addGenre[0];
+        for (GenreID genre : genres) {
+            if(nameForAdd.equals(genre.getGenreDTO().getName())){
+                throw new IllegalArgumentException("Жанр с именем " + nameForAdd + " уже есть в списке жанров");
             }
         }
-        if(mapParametrs.containsKey(ADD)){
-            String[] addGenre = mapParametrs.get(ADD);
-            if(addGenre == null) {
-                throw new IllegalArgumentException("Название нового жанра для добавления не передано");
-            }
-            if(addGenre.length > 1){
-                throw new IllegalArgumentException("Жанры можно добавлять только по одному");
-            }
-            if(addGenre[0] == null || addGenre[0].isBlank()){
-                throw new IllegalArgumentException("Необходимо ввести название жанра для добавления");
-            }
-            String nameForAdd = addGenre[0];
-            for (GenreID genre : genres) {
-                if(nameForAdd.equals(genre.getGenreDTO().getName())){
-                    throw new IllegalArgumentException("Жанр с именем " + nameForAdd + " уже есть в списке жанров");
-                }
-            }
-            service.add(nameForAdd);
-            writer.write("<p>Вы добавили жанр " + nameForAdd + "</p>");
+        service.add(nameForAdd);
+        writer.write("<p>Вы добавили жанр " + nameForAdd + "</p>");
+    }
+    @Override
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setCharacterEncoding("UTF-8");
+        resp.setContentType("text/html; charset=UTF-8");
+
+        Map<String, String[]> mapParametrs = req.getParameterMap();
+
+        List<GenreID> genres = service.get();
+
+        PrintWriter writer = resp.getWriter();
+
+        String[] updateId = mapParametrs.get(UPDATE_ID);
+        String[] newName =  mapParametrs.get(UPDATE_NEW_NAME);
+
+        if(updateId == null && newName == null) {
+            throw new IllegalArgumentException("Id жанра или название нового жанра для обновления не переданы");
         }
-        if(mapParametrs.containsKey(UPDATE_ID) && mapParametrs.containsKey(UPDATE_NEW_NAME)){
-            String[] updateId = mapParametrs.get(UPDATE_ID);
-            String[] newName =  mapParametrs.get(UPDATE_NEW_NAME);
+        if(updateId.length > 1 && newName.length > 1){
+            throw new IllegalArgumentException("Жанры можно обновлять только по одному");
+        }
+        if(updateId[0] == null || updateId[0].isBlank() || newName[0] == null || newName[0].isBlank()){
+            throw new IllegalArgumentException("Обязательно необходимо ввести Id и новое название жанра " +
+                    "для обновления");
+        }
+        if(!NumberUtils.isDigits(updateId[0])){
+            throw new IllegalArgumentException("В поле updateId необходимо ввести число для обновления жанра по id");
+        }
 
-            if(updateId == null && newName == null) {
-                throw new IllegalArgumentException("Id жанра или название нового жанра для обновления не переданы");
-            }
-            if(updateId.length > 1 && newName.length > 1){
-                throw new IllegalArgumentException("Жанры можно обновлять только по одному");
-            }
-            if(updateId[0] == null || updateId[0].isBlank() || newName[0] == null || newName[0].isBlank()){
-                throw new IllegalArgumentException("Обязательно необходимо ввести Id и новое название жанра " +
-                        "для обновления");
-            }
-            if(!NumberUtils.isDigits(updateId[0])){
-                throw new IllegalArgumentException("В поле updateId необходимо ввести число для обновления жанра по id");
-            }
+        long idForUpdate = Integer.parseInt(updateId[0]);
+        String name = newName[0];
 
-            long idForUpdate = Integer.parseInt(updateId[0]);
-            String name = newName[0];
+        if(service.exist(idForUpdate)){
+            service.update(idForUpdate, name);
+            writer.write("<p>Вы обновили название жанра по id: " + idForUpdate + " на название: " + name + "</p>");
+        }
+        else {
+            writer.write("<p>Не удалось обновить название жанра по id: " + idForUpdate + " на название: " + name + "</p>");
+        }
+    }
+    @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setCharacterEncoding("UTF-8");
+        resp.setContentType("text/html; charset=UTF-8");
 
-            if(service.exist(idForUpdate)){
-                service.update(idForUpdate, name);
-                writer.write("<p>Вы обновили название жанра по id: " + idForUpdate + " на название: " + name + "</p>");
-            }
-            else {
-                writer.write("<p>Не удалось обновить название жанра по id: " + idForUpdate + " на название: " + name + "</p>");
-            }
+        Map<String, String[]> mapParametrs = req.getParameterMap();
+
+        List<GenreID> genres = service.get();
+
+        PrintWriter writer = resp.getWriter();
+
+        String[] deleteGenre = mapParametrs.get(DELETE_ID);
+        if(deleteGenre == null) {
+            throw new IllegalArgumentException("id жанра для удаления не передан");
+        }
+        if(deleteGenre.length > 1){
+            throw new IllegalArgumentException("Жанры можно удалять только по одному");
+        }
+        if(deleteGenre[0] == null || deleteGenre[0].isBlank() || !NumberUtils.isDigits(deleteGenre[0])){
+            throw new IllegalArgumentException("Необходимо ввести число, для удаления жанра по id");
+        }
+        int idForDelete = Integer.parseInt(deleteGenre[0]);
+
+        if(service.exist(idForDelete)){
+            service.delete(idForDelete);
+            writer.write("<p>Вы удалили жанр по id: " + idForDelete + "</p>");
         }
     }
 }
