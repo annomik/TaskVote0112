d5a47c7c1af8f351163ac9ba10ab6ffaba42c4cd
Dzmitry Bulavitski <dm_bax@mail.ru>
2023-01-15 22:40:09 +0300

    Добавил метод validation в IGenreService и в GenreService. Подправил GenresServlet

--- src/main/java/by/it_academy/jd2/MJD29522/service/GenreService.java
+++ src/main/java/by/it_academy/jd2/MJD29522/service/GenreService.java
@@ -3,17 +3,13 @@ package by.it_academy.jd2.MJD29522.service;
 import by.it_academy.jd2.MJD29522.dao.api.IGenreDao;
 import by.it_academy.jd2.MJD29522.dto.GenreID;
 import by.it_academy.jd2.MJD29522.service.api.IGenreService;
+
 import java.util.List;
+import java.util.Map;
 
 public class GenreService implements IGenreService {
 
-//    private final IGenreDao dao;
-
     private final IGenreDao dao;
-
-//    public GenreService(IGenreDao dao) {
-//        this.dao = dao;
-//    }
 
     public GenreService(IGenreDao dao) {
         this.dao = dao;
@@ -42,5 +38,28 @@ public class GenreService implements IGenreService {
     @Override
     public boolean exist(long id) {
         return this.dao.exist(id);
+    }
+
+    public boolean validation(Map<String, String[]> mapParametrs, String paramert) {
+
+        List<GenreID> genres = get();
+
+        String[] paramerts = mapParametrs.get(paramert);
+
+        if (paramerts.length > 1) {
+            throw new IllegalArgumentException("Выполнять операции с жанрами можно только по одному. Введите только " +
+                    "один жанр");
+        }
+        if (paramerts[0] == null || paramerts[0].isBlank()) {
+            throw new IllegalArgumentException("Не передан параметр жанра для выполнения операции");
+        }
+
+        String nameForAdd = paramerts[0];
+        for (GenreID genre : genres) {
+            if (nameForAdd.equals(genre.getGenreDTO().getName())) {
+                throw new IllegalArgumentException("Жанр с именем " + nameForAdd + " уже есть в списке жанров");
+            }
+        }
+        return true;
     }
 }
--- src/main/java/by/it_academy/jd2/MJD29522/service/api/IGenreService.java
+++ src/main/java/by/it_academy/jd2/MJD29522/service/api/IGenreService.java
@@ -3,6 +3,7 @@ package by.it_academy.jd2.MJD29522.service.api;
 import by.it_academy.jd2.MJD29522.dto.GenreID;
 
 import java.util.List;
+import java.util.Map;
 
 public interface IGenreService {
 
@@ -15,4 +16,7 @@ public interface IGenreService {
     boolean delete(long id);
 
     boolean exist (long id);
+
+    boolean validation(Map<String, String[]> mapParametrs, String operation);
+
 }
