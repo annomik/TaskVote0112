1671e46a1268a2979dde13423a8fa9bb7710ef3a
Pasha <Horvard777@mail.ru>
2023-01-17 20:55:03 +0300

    ddl

--- postgres/ddl/05_vote_genres.sql
+++ postgres/ddl/05_vote_genres.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS app.vote_genres
 (
     genre_id bigint,
     vote_id bigint,
-    CONSTRAINT vote_genres_genre_id_vote_id_key UNIQUE (genre_id)
+    CONSTRAINT vote_genres_genre_id_vote_id_key UNIQUE (genre_id,vote_id)
         INCLUDE(vote_id),
     CONSTRAINT vote_genres_genre_id_fkey FOREIGN KEY (genre_id)
         REFERENCES app.genres (id) MATCH SIMPLE
