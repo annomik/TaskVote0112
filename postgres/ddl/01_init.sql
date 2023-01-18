5d5aef13c910cf5df5281e4913306fda91897775
Dzmitry Bulavitski <dm_bax@mail.ru>
2023-01-18 17:55:52 +0300

    Переименовал ddl и gitignor

--- postgres/ddl/01_init.sql
+++ postgres/ddl/01_init.sql
@@ -1,9 +1,3 @@
 CREATE SCHEMA IF NOT EXISTS app
     AUTHORIZATION postgres;
 
-CREATE TABLE IF NOT EXISTS app.artists
-(
-    id bigserial,
-    name text NOT NULL,
-    CONSTRAINT artists_pkey PRIMARY KEY (id)
-)
