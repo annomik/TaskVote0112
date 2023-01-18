f1df3b5b0587975bf8ffdb02a1d97751ef5d811e
Dzmitry Bulavitski <dm_bax@mail.ru>
2023-01-19 00:55:00 +0300

    Исправил ошибки по коллекции голосов и баги по полям БД, доделал передачу мейла в БД, переделал запросы sql по получению всей информации из БД.

--- src/main/java/by/it_academy/jd2/MJD29522/dao/VoteDaoDB.java
+++ src/main/java/by/it_academy/jd2/MJD29522/dao/VoteDaoDB.java
@@ -1,6 +1,7 @@
 package by.it_academy.jd2.MJD29522.dao;
 
 import by.it_academy.jd2.MJD29522.dao.api.IVoteDao;
+import by.it_academy.jd2.MJD29522.dao.fabrics.SrartingDBSingleton;
 import by.it_academy.jd2.MJD29522.dto.Vote;
 import by.it_academy.jd2.MJD29522.dto.VoteDTO;
 import by.it_academy.jd2.MJD29522.util.StartingDB;
@@ -10,11 +11,14 @@ import java.time.LocalDate;
 import java.util.*;
 
 public class VoteDaoDB implements IVoteDao {
-    private final String saveVoteSQL = "INSERT INTO app.votes (about, date, email) VALUES (?,now(),'123@mail.ru');";
+
+    StartingDB startingDB;
+    private final String saveVoteSQL = "INSERT INTO app.votes (about, date, email) VALUES (?,now(), ?);";
     private final String saveVoteArtistSQL = "INSERT INTO app.vote_artist(artist_id,vote_id) VALUES (?,?)";
     private final String saveVoteGenresSQL = "INSERT INTO app.vote_genres(genre_id,vote_id) VALUES (?,?)";
     private final String getVoteSQL =
-            "SELECT app.votes.id, about, date, genres_arr.genres, artist_id AS artist " +
+//            "SELECT app.votes.id, about, date, genres_arr.genres, artist_id AS artist " +
+                    "SELECT id, about, date, email, genres, artist_id AS artist " +
             "FROM app.votes " +
                 "FULL OUTER JOIN (" +
                     "SELECT vote_id, array_agg(genre_id) AS genres " +
@@ -22,40 +26,46 @@ public class VoteDaoDB implements IVoteDao {
                     "GROUP BY vote_id) genres_arr " +
                     "ON id=vote_id " +
                 "FULL OUTER JOIN app.vote_artist ON id = app.vote_artist.vote_id;";
+
+    public VoteDaoDB(){
+        this.startingDB = SrartingDBSingleton.getInstance();
+    }
     @Override
     public List<Vote> getVoteList() {
         List<Vote> votes = new LinkedList<>();
-        StartingDB startingDB = new StartingDB();
+
         try {
             Connection connection = startingDB.load();
             PreparedStatement preparedStatement = connection.prepareStatement(getVoteSQL);
             ResultSet resultSet = preparedStatement.executeQuery();
             while (resultSet.next()){
-                long id = resultSet.getLong("app.votes.id");
+//                long id = resultSet.getLong("app.votes.id");
+                long id = resultSet.getLong("id");
                 String about = resultSet.getString("about");
                 LocalDate date = resultSet.getDate("date").toLocalDate();
-                Array array = resultSet.getArray("genres_arr.genres");
+                String email = resultSet.getString("email");
+                Array array = resultSet.getArray("genres");
                 Long[] genresLong = (Long[]) array.getArray();
                 long artist = resultSet.getLong("artist");
                 //------------------------------------------------------------------------------
-                votes.add(new Vote(id,new VoteDTO(artist,arrayLongs(genresLong),about,""),date));
+                votes.add(new Vote(id,new VoteDTO(artist,arrayLongs(genresLong),about, email),date));
                 //------------------------------------------------------------------------------
             }
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
-        return null;
+        return votes;
     }
 
     @Override
     public boolean save(VoteDTO vote) {
         try {
-           StartingDB startingDB = new StartingDB();
            Connection connection = startingDB.load();
            PreparedStatement preparedStatement = connection.prepareStatement(saveVoteSQL,
                    Statement.RETURN_GENERATED_KEYS
            );
            preparedStatement.setString(1,vote.getMessage());
+           preparedStatement.setString(2, vote.getEmail());
            preparedStatement.executeUpdate();
            ResultSet resultSetForID = preparedStatement.getGeneratedKeys();
            resultSetForID.next();
@@ -69,7 +79,6 @@ public class VoteDaoDB implements IVoteDao {
     }
 
     private boolean saveVoteArtist(long id, VoteDTO vote){
-        StartingDB startingDB = new StartingDB();
         try {
             Connection connection = startingDB.load();
             PreparedStatement preparedStatement = connection.prepareStatement(saveVoteArtistSQL);
@@ -83,7 +92,6 @@ public class VoteDaoDB implements IVoteDao {
     }
 
     private boolean saveVoteGenres(long id, VoteDTO voteDTO){
-        StartingDB startingDB = new StartingDB();
         try {
             Connection connection = startingDB.load();
             PreparedStatement preparedStatement = connection.prepareStatement(saveVoteGenresSQL);
