ea265fbfe798840802570786756292a6e3fce347
Pasha <Horvard777@mail.ru>
2023-02-07 00:10:30 +0300

    email dll

--- postgres/ddl/07_email.sql
+++ postgres/ddl/07_email.sql
@@ -1,9 +1,9 @@
     CREATE TABLE IF NOT EXISTS app.email
     (
         id bigint NOT NULL,
-        email character varying(255) COLLATE pg_catalog."default",
+        email character varying(255),
         lastsendtime bigint,
-        massage character varying(255) COLLATE pg_catalog."default",
+        massage character varying(255),
         sendmassage boolean,
         validateemail boolean,
         CONSTRAINT email_pkey PRIMARY KEY (id)
